c687f37b9c10a275f3589012e1776abec205a03d
Ngonyoku <ngonyoku001@gmail.com>
2020-06-07 18:34:33 +0300

    Database Added to App.

---
+++ app/src/main/java/com/RickProjects/notekeeper/LocalDBHandlers/MyNotesDBHelper.java
@@ -0,0 +1,44 @@
+package com.RickProjects.notekeeper.LocalDBHandlers;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyNotesDBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "myNotes.db"; //Database Name
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME = "notes_table"; //Table Name.
+
+    //Columns
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_UUID = "uuid";
+    public static final String COLUMN_TITLE = "note_title";
+    public static final String COLUMN_CONTENTS = "note_contents";
+    public static final String COLUMN_DATE_CREATED = "date_created";
+    public static final String COLUMN_CATEGORY = "note_category";
+
+    public MyNotesDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE " + TABLE_NAME
+                + "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_UUID + ","
+                + COLUMN_TITLE + ","
+                + COLUMN_CONTENTS + ","
+                + COLUMN_DATE_CREATED + " ,"
+                + COLUMN_CATEGORY
+                + ")"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
--- app/src/main/java/com/RickProjects/notekeeper/LocalDBHandlers/Operation.java
+++ app/src/main/java/com/RickProjects/notekeeper/LocalDBHandlers/Operation.java
@@ -1,24 +1,64 @@
 package com.RickProjects.notekeeper.LocalDBHandlers;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 import com.RickProjects.notekeeper.Models.Notes;
 
-import java.text.DateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.util.UUID;
 
 public class Operation {
     private static Operation instance;
-    private List<Notes> notesList;
+    //    private List<Notes> notesList;
+    private SQLiteDatabase myDatabase;
 
     private Operation(Context context) {
-        notesList = new ArrayList<>();
+//        notesList = new ArrayList<>();
+        myDatabase = new MyNotesDBHelper(context).getWritableDatabase();
+
     }
 
     public List<Notes> getListOfNotes() {
+//        return notesList;
+        Cursor cursor = myDatabase.query(
+                MyNotesDBHelper.TABLE_NAME,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        List<Notes> notesList = new ArrayList<>();
+
+        try {
+            cursor.moveToFirst();
+
+            while (!cursor.isAfterLast()) {
+                String ID = cursor.getString(cursor.getColumnIndex(MyNotesDBHelper.COLUMN_UUID));
+                String title = cursor.getString(cursor.getColumnIndex(MyNotesDBHelper.COLUMN_TITLE));
+                String contents = cursor.getString(cursor.getColumnIndex(MyNotesDBHelper.COLUMN_CONTENTS));
+                String dateCreated = cursor.getString(cursor.getColumnIndex(MyNotesDBHelper.COLUMN_DATE_CREATED));
+                String category = cursor.getString(cursor.getColumnIndex(MyNotesDBHelper.COLUMN_CATEGORY));
+
+                Notes notes = new Notes(UUID.fromString(ID));
+                notes.setNoteTitle(title);
+                notes.setNoteCategory(category);
+                notes.setNoteContents(contents);
+                notes.setDateCreated(dateCreated);
+
+                notesList.add(notes);
+                cursor.moveToNext();
+            }
+        } finally {
+            cursor.close();
+        }
+
         return notesList;
     }
 
@@ -31,36 +71,89 @@ public class Operation {
     }
 
     public void updateNote(UUID ID, String title, String contents, String dateEdited/*,String category*/) {
-        for (Notes notes : notesList) {
-            if (notes.getIdentifier().equals(ID)) {
-                notes.setNoteTitle(title);
-                notes.setNoteContents(contents);
-                notes.setDateCreated(dateEdited);
-//                notes.setNoteCategory(category);
-                break;
-            }
-        }
+//        for (Notes notes : notesList) {
+//            if (notes.getIdentifier().equals(ID)) {
+//                notes.setNoteTitle(title);
+//                notes.setNoteContents(contents);
+//                notes.setDateCreated(dateEdited);
+////                notes.setNoteCategory(category);
+//                break;
+//            }
+//        }
+        Notes notes = new Notes(ID);
+        notes.setNoteTitle(title);
+        notes.setNoteContents(contents);
+        notes.setDateCreated(dateEdited);
+        myDatabase.update(
+                MyNotesDBHelper.TABLE_NAME,
+                getContentValues(notes),
+                MyNotesDBHelper.COLUMN_UUID + " = ?",
+                new String[]{ID.toString()}
+        );
     }
 
     public void deleteNote(UUID ID) {
-        for (Notes notes : notesList) {
-            if (notes.getIdentifier().equals(ID)) {
-                notesList.remove(notes);
-            }
-        }
+//        for (Notes notes : notesList) {
+//            if (notes.getIdentifier().equals(ID)) {
+//                notesList.remove(notes);
+//            }
+//        }
+
+        myDatabase.delete(
+                MyNotesDBHelper.TABLE_NAME,
+                MyNotesDBHelper.COLUMN_UUID + " = ?",
+                new String[]{ID.toString()}
+        );
     }
 
     public Notes getNote(UUID ID) {
-        for (Notes notes : notesList) {
-            if (notes.getIdentifier().equals(ID)) {
-                return notes;
-            }
-        }
+//        for (Notes notes : notesList) {
+//            if (notes.getIdentifier().equals(ID)) {
+//                return notes;
+//            }
+//        }
+//
+//        return null;
+        Cursor cursor = myDatabase.query(
+                MyNotesDBHelper.TABLE_NAME,
+                null,
+                MyNotesDBHelper.COLUMN_UUID + " = ?",
+                new String[]{ID.toString()},
+                null,
+                null,
+                null
+        );
+        try {
+            cursor.moveToFirst();
+            Notes notes = new Notes(ID);
+            notes.setNoteTitle(cursor.getString(cursor.getColumnIndex(MyNotesDBHelper.COLUMN_TITLE)));
+            notes.setNoteContents(cursor.getString(cursor.getColumnIndex(MyNotesDBHelper.COLUMN_CONTENTS)));
+            notes.setDateCreated(cursor.getString(cursor.getColumnIndex(MyNotesDBHelper.COLUMN_DATE_CREATED)));
+            notes.setNoteCategory(cursor.getString(cursor.getColumnIndex(MyNotesDBHelper.COLUMN_CATEGORY)));
 
-        return null;
+            return notes;
+        } finally {
+            cursor.close();
+        }
     }
 
     public void addNote(Notes notes) {
-        notesList.add(notes);
+//        notesList.add(notes);
+        myDatabase.insert(
+                MyNotesDBHelper.TABLE_NAME,
+                null,
+                getContentValues(notes)
+        );
+    }
+
+    private ContentValues getContentValues(Notes notes) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MyNotesDBHelper.COLUMN_UUID, notes.getIdentifier().toString());
+        contentValues.put(MyNotesDBHelper.COLUMN_TITLE, notes.getNoteTitle());
+        contentValues.put(MyNotesDBHelper.COLUMN_CONTENTS, notes.getNoteContents());
+        contentValues.put(MyNotesDBHelper.COLUMN_CATEGORY, notes.getNoteCategory());
+        contentValues.put(MyNotesDBHelper.COLUMN_DATE_CREATED, notes.getDateCreated());
+
+        return contentValues;
     }
 }
