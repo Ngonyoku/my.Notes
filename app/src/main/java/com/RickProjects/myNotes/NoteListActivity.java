f3e3ccf9593da391903d1c0a97f13be2a379bee2
Ngonyoku <ngonyoku001@gmail.com>
2020-07-13 03:51:02 +0300

    Components Added

--- app/src/main/java/com/RickProjects/myNotes/NoteListActivity.java
+++ app/src/main/java/com/RickProjects/myNotes/NoteListActivity.java
@@ -6,11 +6,34 @@ import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
+    private NoteViewModel mNoteViewModel;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_note_list);
+        NoteListAdapter adapter = new NoteListAdapter();
+        mNoteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mNoteViewModel.getNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                adapter.submitList(notes);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/RickProjects/myNotes/NoteListAdapter.java
@@ -0,0 +1,65 @@
+package com.RickProjects.myNotes;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NoteListAdapter extends ListAdapter<Note, NoteListAdapter.NoteListViewHolder> {
+    protected NoteListAdapter() {
+        super(DIFF_CALL_BACK);
+    }
+
+    private static final DiffUtil.ItemCallback<Note> DIFF_CALL_BACK = new DiffUtil.ItemCallback<Note>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getId() == newItem.getId();
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Note oldItem, @NonNull Note newItem) {
+            return oldItem.getDateCreated().equals(newItem.getDateCreated())
+                    && oldItem.getDescription().equals(newItem.getDescription())
+                    && oldItem.getTitle().equals(newItem.getTitle());
+        }
+    };
+
+
+    @NonNull
+    @Override
+    public NoteListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new NoteListViewHolder(
+                LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_note_list, parent, false)
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteListViewHolder holder, int position) {
+        Note currentNote = getItem(position);
+        holder.textViewTitle.setText(currentNote.getTitle());
+        holder.textViewDescription.setText(currentNote.getDescription());
+        holder.textViewDate.setText(currentNote.getDateCreated());
+    }
+
+    public Note getNoteAt(int position) {
+        return getItem(position);
+    }
+
+    class NoteListViewHolder extends RecyclerView.ViewHolder {
+        private TextView textViewTitle, textViewDescription, textViewDate;
+
+        public NoteListViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.textView_noteTitle);
+            textViewDescription = itemView.findViewById(R.id.textView_description);
+            textViewDate = itemView.findViewById(R.id.textView_date);
+        }
+    }
+}
